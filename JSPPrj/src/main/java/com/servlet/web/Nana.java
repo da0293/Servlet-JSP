eceb5d33c5081b09afc75c6b99313b7b895918d6
da0293 <dy7133@naver.com>
2024-02-26 21:50:17 +0900

    commit 기본 get요청

--- JSPPrj/src/main/java/com/servlet/web/Nana.java
+++ JSPPrj/src/main/java/com/servlet/web/Nana.java
@@ -13,13 +13,15 @@ import javax.servlet.http.HttpServletResponse;
 public class Nana extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// 사용자 본연의 코딩인코딩방식 설정(파일을 UTF-8로 보냄)
 		resp.setCharacterEncoding("UTF-8");
-		// 사용자가 어떻게 읽어야할지 알려줌
-		// 브라우저에게 charset=UTF-8로 해석하라고 알려줌 
 		resp.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = resp.getWriter();
-		for( int i=0; i<10; i++ ) {
+		String cnt_=req.getParameter("cnt"); 
+		int cnt=10; 
+		if(cnt_!=null && !cnt_.equals("")) {
+			cnt=Integer.parseInt(cnt_);
+		}
+		for( int i=0; i<cnt; i++ ) {
 			out.println((i+1)+": 안녕 Servlet <br>");
 		}
 	}
