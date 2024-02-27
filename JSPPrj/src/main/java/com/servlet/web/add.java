4a6df4833fc0c650c75b593f70e41948fc0889cf
da0293 <dy7133@naver.com>
2024-02-27 22:36:16 +0900

    commit 여러개 submit버튼 사용

--- JSPPrj/src/main/java/com/servlet/web/add.java
+++ JSPPrj/src/main/java/com/servlet/web/add.java
@@ -9,18 +9,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/notice-reg")
-public class NoticeReg extends HttpServlet{
+@WebServlet("/add")
+public class add extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setCharacterEncoding("UTF-8");
 		resp.setContentType("text/html; charset=UTF-8");
-		// 입력값을 UTF-8로 읽음
-		req.setCharacterEncoding("UTF-8"); 
-		PrintWriter out = resp.getWriter();
-		String title=req.getParameter("title"); 
-		String content=req.getParameter("content"); 
-		out.println(title);
-		out.println(content); 
+		PrintWriter out = resp.getWriter(); 
+		String x_ = req.getParameter("x"); 
+		String y_ = req.getParameter("y");
+		int x=0; 
+		int y=0; 
+		if(x_!=null && y_!=null && !x_.equals("") && !y_.equals("")) {
+			x=Integer.parseInt(x_); 
+			y=Integer.parseInt(y_); 
+		}
+		out.println("계산 결과는 : " + (x+y));
 	}
 }
