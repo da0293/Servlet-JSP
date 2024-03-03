5a52102be725cbe52dcbf7fcb0fe3bcedd3f4bc0
da0293 <dy7133@naver.com>
2024-03-03 20:44:01 +0900

    commit 계싼기 쿠키삭제까지 완료

--- JSPPrj/src/main/java/com/servlet/web/Calc2.java
+++ JSPPrj/src/main/java/com/servlet/web/Calc2.java
@@ -16,11 +16,6 @@ import javax.servlet.http.HttpSession;
 public class Calc2 extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// ServletContext : Application저장소
-		ServletContext application = req.getServletContext();
-		// Session객체
-		HttpSession session = req.getSession();
-		// 쿠키 배열
 		Cookie[] cookies = req.getCookies(); 
 		
 		resp.setCharacterEncoding("UTF-8");
@@ -28,19 +23,12 @@ public class Calc2 extends HttpServlet{
 		String v_=req.getParameter("v"); 
 		String op = req.getParameter("operator");
 		
-		// 기본값(값을 입력하지 않은 경우)
 		int v=0; 
 		int result=0; 
 		if(!v_.equals("")) v = Integer.parseInt(v_);
-		
-		// op에 따라 값을 계산하거나 저장한다.
-		// 계산
+
 		if(op.equals("=")) { // 계산
-			// x는 앞에서 저장했던값(Application저장소에 담겨진 내용)
-			// int x=(Integer)application.getAttribute("value"); 
-			// int x=(Integer)session.getAttribute("value"); 
 			int x=0; 
-			// 내가 심어놨던  value쿠키 키값 찾기
 			for(Cookie c :  cookies) {
 				if(c.getName().equals("value")) {
 					x = Integer.parseInt(c.getValue());
@@ -68,22 +56,18 @@ public class Calc2 extends HttpServlet{
 		}
 		// 저장
 		else { // 기본연산을 저장
-			// Application에 v와 op를 저장 (Map Collection과 유사)
-			//application.setAttribute("value", v); 
-			//application.setAttribute("op", op); 
-			
-			// session.setAttribute("value", v); 
-			// session.setAttribute("op", op);
-			
-			// 1. 
-			// 쿠키값은 문자열로만 보내야 한다. 
 			Cookie valueCookie = new Cookie("value", String.valueOf(v)); 
 			Cookie opCookie = new Cookie("op", op); 
 			// 쿠키가 어느 경우에 사용자에게 전달되어야하는지 경로 설정
-			valueCookie.setPath("/add"); 
-			opCookie.setPath("/add"); 
+			valueCookie.setPath("/calc2");
+			opCookie.setPath("/calc2"); 
+			// 앞으로 1분 후에 valueCookie 만료 
+			valueCookie.setMaxAge(60*60); 
+			// 쿠키를 클라이언트에게 전달 
 			resp.addCookie(valueCookie); 
 			resp.addCookie(opCookie); 
+			
+			resp.sendRedirect("calc2.html"); 
 		}
 	}
 }
---
+++ JSPPrj/src/main/java/com/servlet/web/Calc3.java
@@ -0,0 +1,75 @@
+package com.servlet.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.swing.RepaintManager;
+
+@WebServlet("/calc3")
+public class Calc3 extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 클라이언트로부터 전송된 쿠키를 읽어옴
+        Cookie[] cookies = req.getCookies();
+        
+        // 사용자가 입력한 값(value), 연산자(operator), 소수점(dot)을 가져옴
+        String value = req.getParameter("value");
+        String operator = req.getParameter("operator");
+        String dot = req.getParameter("dot");
+
+        // 이전에 저장된 계산식을 읽어옴
+        String exp = "";
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+                if (c.getName().equals("exp")) {
+                    exp = c.getValue();
+                    break;
+                }
+            }
+        }
+
+        // '=' 연산자가 들어온 경우, 계산식(exp)을 처리
+        if (operator != null && operator.equals("=")) {
+        	// Graal.js 엔진을 사용하여 계산식 처리
+        	ScriptEngine engine = new ScriptEngineManager().getEngineByName("graal.js"); 
+        	try {
+        		// 계산식을 평가하여 결과를 문자열로 변환
+        		exp = String.valueOf(engine.eval(exp));
+			} catch (ScriptException e) {
+				// 스크립트 평가 중 오류가 발생한 경우
+				e.printStackTrace();
+			} 
+        } else if(operator !=null && operator.equals("C")) {
+        	// 
+        	exp=""; 
+        }else {
+            // 입력값(value), 연산자(operator), 소수점(dot)을 기존의 계산식에 추가
+            exp += (value == null) ? "" : value;
+            exp += (operator == null) ? "" : operator;
+            exp += (dot == null) ? "" : dot;
+        }
+        
+
+        // 계산식을 쿠키로 저장
+        Cookie expCookie = new Cookie("exp", exp);
+        // C 누를 시 초기화(쿠키 삭제)
+        if(operator!=null && operator.equals("C")) {
+            expCookie.setMaxAge(0);
+        }
+        resp.addCookie(expCookie);
+        // "calcpage"로 리다이렉트
+        resp.sendRedirect("calcpage");
+    }
+}
+
